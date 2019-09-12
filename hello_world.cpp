45ed318edc5c765ba3e9cc612ce2f8037194a0bc
Ubuntu <ubuntu@ip-172-31-27-73.ec2.internal>
2019-09-12 21:23:34 +0000

    hello world proper format

---
+++ hello_world.cpp
@@ -0,0 +1,10 @@
+// Copyright (c) 2019 Jacob Bonner All rights reserved.
+//
+// Created by: Jacob Bonner
+// Created on: September 2019
+// The "Hello, World!" program in proper format
+
+#include <iostream>
+
+int main()
+{std::cout << "Hello, World!" << std::endl;}
